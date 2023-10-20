ee14212df8e2e8177b8e40b673dc10e528b9a30a
Invader-Faisi <code.pedia.tube@gmail.com>
2023-10-21 00:56:56 +0500

    filament actions done

--- app/Filament/Resources/ProductResource.php
+++ app/Filament/Resources/ProductResource.php
@@ -38,6 +38,8 @@ class ProductResource extends Resource
 
     protected static ?string $navigationGroup = 'Shop';
 
+    protected static ?int $navigationSort = 0;
+
     public static function form(Form $form): Form
     {
         return $form
@@ -161,7 +163,11 @@ class ProductResource extends Resource
                     ->relationship('brand', 'name')
             ])
             ->actions([
-                Tables\Actions\EditAction::make(),
+                Tables\Actions\ActionGroup::make([
+                    Tables\Actions\ViewAction::make(),
+                    Tables\Actions\EditAction::make(),
+                    Tables\Actions\DeleteAction::make(),
+                ])
             ])
             ->bulkActions([
                 Tables\Actions\BulkActionGroup::make([
